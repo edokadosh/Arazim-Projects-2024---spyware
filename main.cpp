3ca9d78d61e0c5713b19a27828f0b0935a6986c5
sivan <you@example.com>
2024-05-22 21:37:29 +0300

    starting work on main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
 #include "Errors.h"
+#include "client.h"
 #include "SoftwareManeger.h"
 
 void testSoftwareManeger(void);
 
 int main(int argc, char * argv[]) {
-    testSoftwareManeger();
+    
+    Client client;
+    client.createSocket();
+    SoftwareManeger swm = SoftwareManeger();
+
+    while (true)
+    {
+        loopIter(client, swm);
+    }
+
     return 0;
 }
 
@@ -17,4 +27,26 @@ void testSoftwareManeger() {
     
     swm.deleteFile("tempSwmTest.txt");
 
+}
+
+void loopIter(Client& client, SoftwareManeger& swm)
+{
+    // receive command
+    Message msg;
+    client.recvCommand(msg);
+    int fnccode = msg.fnccode();
+    std::string command = msg.param1();
+
+    std::string response = "";
+    switch (fnccode)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+    
+    // return answer
 }
