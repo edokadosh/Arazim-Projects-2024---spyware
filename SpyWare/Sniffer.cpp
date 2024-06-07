fd72c95a6e1e47070640f8cd94a1f24b8b8be7e3
sivan <you@example.com>
2024-06-07 21:18:13 +0300

    erjfu

--- SpyWare/Sniffer.cpp
+++ SpyWare/Sniffer.cpp
@@ -22,14 +22,6 @@ bool callback(const PDU &pdu) {
     cout << "size: " << ip.size() << " | ";
     cout << ip.src_addr() << ':' << tcp.sport() << " -> " 
          << ip.dst_addr() << ':' << tcp.dport() << endl;
-    
-    if (tcp.sport() == 80 || tcp.dport() == 80)
-    {
-        const RawPDU* pkt = pdu.find_pdu<RawPDU>();
-        RawPDU::payload_type payload = pkt->payload();
-        vector<uint8_t> dt = payload.data();
-        cout << "HTTP: " <<  payload << endl;
-    }
 
     return true;
 }
