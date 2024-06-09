bb06b60f9dde0d4cc5e920a7449ecd30a7c82461
sivan <you@example.com>
2024-06-09 19:48:20 +0300

    sniffer with adapter

--- SpyWare/ParamStructs.h
+++ SpyWare/ParamStructs.h
@@ -1,9 +1,12 @@
 #ifndef PARAM_STRUCTS_H
 #define PARAM_STRUCTS_H
 
+#define MAX_ADAPTER_NAMELEN (16)
+
 typedef struct SniffParams {
 
     int time;
+    char adapter[MAX_ADAPTER_NAMELEN];
 
 } SniffParams;
 
--- SpyWare/SpyWare.cpp
+++ SpyWare/SpyWare.cpp
@@ -3,9 +3,12 @@
 int main() {
     Sniffer s = Sniffer();
     ContParams c;
-    c.parameters.snifP.time = 30;
+    c.parameters.snifP.time = 5;
+    strncpy(c.parameters.snifP.adapter, "ens33", MAX_ADAPTER_NAMELEN);
+    c.parameters.snifP.adapter[MAX_ADAPTER_NAMELEN - 1] = 0;
     c.type = SnifferType;
 
+    std::cout << "Adapter: " << c.parameters.snifP.adapter << std::endl;
     s.run(c);
 
     return 0;
