1031915fc871899299b1c4d8356bc938658a4f84
avnerspr <avnerspr@gmail.com>
2024-06-17 14:20:29 +0300

    Connection should work for every fd

--- src/Maneger/Connection.cpp
+++ src/Maneger/Connection.cpp
@@ -3,10 +3,11 @@
 #include "Connection.h"
 #include "responce.h"
 
-Connection::Connection(int fdInput, int fdOutput, bool needCloseInput, bool needCloseOutput)
+Connection::Connection(int fdInput, int fdOutput, bool needCloseInput, bool needCloseOutput, bool isSock)
 {
     fdIn = fdInput;
     fdOut = fdOutput;
+    isSocket = isSock;
 
     needCloseIn = needCloseInput;
     if (fdIn != fdOut) {
@@ -24,6 +25,30 @@ Connection::~Connection() {
     // needCloseIn = false;
 }
 
+int Connection::doSend(const void* buf, size_t size, int flags) {
+    if (isSocket)
+    {
+        return ::send(fdOut, buf, size, flags);
+    }
+    return write(fdOut, buf, size);
+}
+
+int Connection::doSend(const void* buf, size_t size) {
+    return write(fdOut, buf, size);
+}
+
+int Connection::doRecv(void* buf, size_t size, int flags) {
+    if (isSocket)
+    {
+        return ::recv(fdOut, buf, size, flags);
+    }
+    return read(fdOut, buf, size);
+}
+
+int Connection::doRecv(void* buf, size_t size) {
+    return read(fdOut, buf, size);
+}
+
 void Connection::closeConnection() {
     if(needCloseIn)
         close(fdIn);
@@ -33,8 +58,8 @@ void Connection::closeConnection() {
 }
 
 
-bool Connection::sendData(uint32_t size, void* buffer) {
-    int bytesSent = ::send(fdOut, buffer, size, 0);
+bool Connection::sendData(uint32_t size, void* buffer, int flags) {
+    int bytesSent = doSend(buffer, size, flags);
     if (bytesSent == -1) {
         std::cerr << "Error sending data" << std::endl;
         std::cerr << "Error: " << strerror(errno) << std::endl;
@@ -43,8 +68,12 @@ bool Connection::sendData(uint32_t size, void* buffer) {
     return true;
 }
 
-bool Connection::sendString(const std::string& str) {
-    int bytesSent = ::send(fdOut, str.c_str(), str.length(), 0);
+bool Connection::sendData(uint32_t size, void* buffer) {
+    return sendData(size, buffer, 0);
+}
+
+bool Connection::sendString(const std::string& str, int flags) {
+    int bytesSent = doSend(str.c_str(), str.length(), flags);
     if (bytesSent == -1) {
         std::cerr << "Error sending string" << std::endl;
         std::cerr << "Error: " << strerror(errno) << std::endl;
@@ -52,10 +81,12 @@ bool Connection::sendString(const std::string& str) {
     }
     return true;
 }
+bool Connection::sendString(const std::string& str) {
+    return sendString(str, 0);
+}
 
-
-bool Connection::sendResponceStruct(const responce res) {
-    int bytesSent = ::send(fdOut, &res, sizeof(res), 0);
+bool Connection::sendResponceStruct(const responce res, int flags) {
+    int bytesSent = doSend(&res, sizeof(res), flags);
     if (bytesSent == -1) {
         std::cerr << "Error sending responce" << std::endl;
         std::cerr << "Error: " << strerror(errno) << std::endl;
@@ -64,22 +95,29 @@ bool Connection::sendResponceStruct(const responce res) {
     return true;
 }
 
-bool Connection::sendResponce(uint32_t status, const std::string& msg) {
+bool Connection::sendResponceStruct(const responce res) {
+    return sendResponceStruct(res, 0);
+}
+
+bool Connection::sendResponce(uint32_t status, const std::string& msg, int flags) {
     uint32_t dataLen = msg.length();
 
-    if(!sendResponceStruct((responce){.dataLen = dataLen, .status = status})) {
+    if(!sendResponceStruct((responce){.dataLen = dataLen, .status = status}, flags)) {
         return false;
     }
-    if (!sendString(msg)) {
+    if (!sendString(msg), flags) {
         return false;
     }
     return true;
 }
 
+bool Connection::sendResponce(uint32_t status, const std::string& msg) {
+    return sendResponce(status, msg, 0);
+}
 
-bool Connection::recvCommand(command& cmd)
+bool Connection::recvCommand(command& cmd, int flags)
 {
-    if (recv(fdIn, &cmd, sizeof(cmd), 0) != sizeof(cmd)) {
+    if (doRecv(&cmd, sizeof(cmd), flags) != sizeof(cmd)) {
         std::cerr << "Receive command failed" << std::endl;
         std::cerr << "Error: " << strerror(errno) << std::endl;
         return false;
@@ -88,11 +126,13 @@ bool Connection::recvCommand(command& cmd)
     return true;
 }
 
+bool Connection::recvCommand(command& cmd) {
+    return recvCommand(cmd, 0);
+}
 
-
-bool Connection::recvData(uint32_t size, char* buffer)
+bool Connection::recvData(uint32_t size, char* buffer, int flags)
 {
-    if (recv(fdIn, buffer, size, 0) == -1) {
+    if (doRecv(buffer, size, flags) == -1) {
         std::cerr << "Receive data failed" << std::endl;
         std::cerr << "Error: " << strerror(errno) << std::endl;
         return false;
@@ -100,7 +140,12 @@ bool Connection::recvData(uint32_t size, char* buffer)
     return true;
 }
 
-bool Connection::sendFile(std::string filePath)
+bool Connection::recvData(uint32_t size, char* buffer) {
+    return recvData(size, buffer, 0);
+}
+
+
+bool Connection::sendFile(std::string filePath, int flags)
 {
     std::ifstream file(filePath, std::ios::binary);
     if (!file.is_open()) {
@@ -109,17 +154,22 @@ bool Connection::sendFile(std::string filePath)
     }
     const std::vector<char> buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     uint32_t fileSize = buffer.size();
-    if ((::send(fdOut, &fileSize, sizeof(fileSize), MSG_MORE)) == -1) {
+    if ((doSend(&fileSize, sizeof(fileSize), MSG_MORE | flags)) == -1) {
         std::cerr << "Error msg length" << std::endl;
         std::cerr << "Error: " << strerror(errno) << std::endl;
     }
     file.close();
 
-    if ((::send(fdOut, buffer.data(), fileSize, 0)) == -1) {
+    if ((doSend(buffer.data(), fileSize, flags)) == -1) {
         std::cerr << "Error msg length" << std::endl;
         std::cerr << "Error: " << strerror(errno) << std::endl;
     }
 
     return true;
 }
+
+bool Connection::sendFile(std::string filePath) {
+    return sendFile(filePath, 0);
+}
+
 
--- src/Maneger/Connection.h
+++ src/Maneger/Connection.h
@@ -20,33 +20,48 @@ private:
     bool needCloseIn;
     bool needCloseOut;
 
+    bool isSocket;
 
-public:
     int fdIn;
     int fdOut;
 
-    Connection(int fdInput, int fdOutput, bool needCloseInput, bool needCloseOutput);
+    int doSend(const void* buf, size_t size, int flags);
+    int doSend(const void* buf, size_t size);
+    int doRecv(void* buf, size_t size, int flags);
+    int doRecv(void* buf, size_t size);
+
+public:
+
+    Connection(int fdInput, int fdOutput, bool needCloseInput, bool needCloseOutput, bool isSocket);
 
     Connection();
 
     ~Connection();
 
     bool sendString(const std::string& str);
+    bool sendString(const std::string& str, int flags);
 
     bool sendResponceStruct(const responce res);
+    bool sendResponceStruct(const responce res, int flags);
 
-    bool sendResponce(uint32_t status, const std::string&);
+    bool sendResponce(uint32_t status, const std::string& ansString);
+    bool sendResponce(uint32_t status, const std::string& ansString, int flags);
 
     bool sendData(uint32_t size, void* buffer);
+    bool sendData(uint32_t size, void* buffer, int flags);
 
     bool recvCommand(command& cmd);
+    bool recvCommand(command& cmd, int flags);
 
     bool recvData(uint32_t size, char data[]);
+    bool recvData(uint32_t size, char data[], int flags);
 
     bool sendFile(std::string filePath);
+    bool sendFile(std::string filePath, int flags);
 
     void closeConnection();
 
+friend class HiderManeger;
 };
 
 #endif
