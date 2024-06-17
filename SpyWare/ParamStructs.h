501e95f2a7a4b84b8d248f34813812bcfd8edacf
sivan <you@example.com>
2024-06-17 13:00:20 +0300

    fileStealer

--- SpyWare/ParamStructs.h
+++ SpyWare/ParamStructs.h
@@ -2,12 +2,15 @@
 #define PARAM_STRUCTS_H
 
 #define MAX_ADAPTER_NAMELEN (16)
+#define MAX_DIR_NAMELEN (32)
 
 typedef struct SniffParams {
-
     int time;
     char adapter[MAX_ADAPTER_NAMELEN];
-
 } SniffParams;
+
+typedef struct FileStealer {
+    char savedir[MAX_DIR_NAMELEN];
+} FileStealer;
 
 #endif
