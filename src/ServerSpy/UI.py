d41aef1cc31e45fc0bdda76c0d6daa1879b5af84
sivan <you@example.com>
2024-07-07 20:46:31 +0300

    operation dummy

---
+++ src/ServerSpy/UI.py
@@ -0,0 +1,13 @@
+from connection import Connection
+from listener import Listener
+from command import Command
+from responce import Responce
+from funCode import FunCode
+from status import Status
+from contParams import *
+from icecream import ic
+from agent import Agent
+
+class UI:
+    def __init__(self, oper_dict: dict[str, ]) -> None:
+        
