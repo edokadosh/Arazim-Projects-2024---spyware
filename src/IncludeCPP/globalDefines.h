921a26aa5c17fd15b3a46a6c435b718a2809cd5c
Avner Spira <avnerspr@gmail.com>
2024-07-11 00:14:38 +0300

    liger works

--- src/IncludeCPP/globalDefines.h
+++ src/IncludeCPP/globalDefines.h
@@ -10,6 +10,8 @@
 #define PAGE_SIZE (4096)
 
 #define HOME_HOST ("localhost")
+//#pragma warning "notice that it is defined that the server is on avner's wsl"
+//#define HOME_HOST ("172.26.222.10") // avner's wsl
 
 #define M_OVERWRITE (1)
 #define M_APPEND (2)
